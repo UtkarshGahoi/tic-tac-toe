d6040681f269256e858a625f7abd884dd3a042bc
up79-code <utkarsh.gahoi@kuliza.com>
2023-11-07 17:32:43 +0530

    tic tac toe

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Player {
+    private String name;
+    private String piece;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPiece() {
+        return piece;
+    }
+
+    public Player(String name, String piece){
+        this.name = name;
+        this.piece = piece;
+    }
+}
